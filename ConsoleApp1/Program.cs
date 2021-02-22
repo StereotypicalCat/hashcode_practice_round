e9fd9971b76e9174da4c351ad83f611a5ab15881
Lucas Winther <lucasw89@live.dk>
2021-02-22 20:53:44 +0100

    Started on naive solution.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        public int bestScore;
+        public int bestDelivery;
+        
         static void Main(string[] args)
         {
             var teams = new int[]
@@ -19,10 +23,56 @@ namespace ConsoleApp1
                 new Pizza(new []{"tomato", "mushroom", "basil"}),
                 new Pizza(new []{"chicken", "basil"})
             };
-            
-
-
 
         }
+
+        static void solve(int[] teams, Pizza[] pizzas, Delivery[] deliveries)
+        {
+            for (int i = 0; i < teams[0]; i++)
+            {
+                for (int j = i; j < pizzas.Length; j++)
+                {
+                    for (int k = j; k < pizzas.Length; k++)
+                    {
+                        // Lav en løsning
+                        
+                        solve(teams, pizzas, deliveries);
+                    }
+                }
+            }
+            
+            for (int i = 0; i < teams[1]; i++)
+            {
+                for (int j = 0; j < pizzas.Length; j++)
+                {
+                    for (int k = 0; k < pizzas.Length; k++)
+                    {
+                        for (int l = 0; l < pizzas.Length; l++)
+                        {
+                            // Lav en løsning
+                            solve(teams, pizzas, deliveries);
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < teams[2]; i++)
+            {
+                for (int j = 0; j < pizzas.Length; j++)
+                {
+                    for (int k = 0; k < pizzas.Length; k++)
+                    {
+                        for (int l = 0; l < pizzas.Length; l++)
+                        {
+                            for (int m = 0; m < pizzas.Length; m++)
+                            {
+                                // Lav en løsning
+                                solve(teams, pizzas, deliveries);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 }
